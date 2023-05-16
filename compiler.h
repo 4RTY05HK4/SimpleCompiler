002dc66d7aa565c982b508be9c778a65205dbfa4
4RT3M <33829671+4RTY05HK4@users.noreply.github.com>
2023-05-16 12:29:03 +0200

    added compiler.h and struct to hold tokens

---
+++ compiler.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <vector>
+
+typedef struct TtokenSet {
+	std::string token;
+	const int tk_type;
+	const int line;
+	const int token_start_pos;
+} TtokenSet;
+
+class CCompiler {
+
+
+
+
+};
--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #pragma warning(disable :4996)
 
 #include "tokenizer.h"
+#include "compiler.h"
 
 int main() {
 
@@ -15,12 +16,17 @@ int main() {
 		std::cout << "File Err" << std::endl;
 		return -1;
 	}
-
+	std::cout << "File size: " << tokenizer.buf2Length << std::endl;
+	std::vector<TtokenSet> tokenSet;
 	std::cout << "Class		Line	Pos.	Content" << std::endl;
 	while (tokenizer.getToken() == 0 && tokenizer.tk_type != TK_EMPTY)
 	{
 		//if (tokenizer.tk_type != 7) std::cout << TKset[tokenizer.tk_type].ident << "        " << tokenizer.line << "        " << tokenizer.token_start_pos << "        " << tokenizer.token << std::endl;
-		if (tokenizer.tk_type != 7) printf("%s		%d	%d	%s \n", TKset[tokenizer.tk_type].ident, tokenizer.line, tokenizer.token_start_pos, tokenizer.token);
+		if (tokenizer.tk_type != 7)
+		{
+			printf("%s		%d	%d	%s \n", TKset[tokenizer.tk_type].ident, tokenizer.line, tokenizer.token_start_pos, tokenizer.token);
+			tokenSet.push_back({tokenizer.token, tokenizer.tk_type, tokenizer.line, tokenizer.token_start_pos});
+		}
 		if (tokenizer.consumeToken() != 0)
 		{
 			std::cout << "Token Err" << std::endl;
@@ -30,6 +36,10 @@ int main() {
 
 	tokenizer.Done();
 
+	for (auto a : tokenSet) {
+		printf("%d	%s	%d	%d \n", a.tk_type, a.token.c_str(), a.line, a.token_start_pos);
+	}
+	
 	getchar();
 	return 0;
 }
