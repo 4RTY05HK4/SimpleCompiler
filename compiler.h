5f16ee6fc3147c4219b95e77f94622bf2901322d
4RT3M <33829671+4RTY05HK4@users.noreply.github.com>
2023-05-23 12:50:55 +0200

    Added expression parsing
    - added support for +, -, *, /, (, ) operators

--- compiler.h
+++ compiler.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <vector>
+#include <unordered_map>
+#include <string>
 
 typedef struct TtokenSet {
 	std::string token;
@@ -10,8 +12,125 @@ typedef struct TtokenSet {
 } TtokenSet;
 
 class CCompiler {
+private:
+
+public:
+	std::vector<TtokenSet> tokenSet;
+	std::unordered_map<std::string, double> vars;
+	int lastLine;
+	int aotil;
+	int tokenIter;
+	int currToken;
+	
+	void isAssign()
+	{
+		lastLine = tokenSet[tokenSet.size()-1].line;
+		for (int i = 1; i <= lastLine; i++)
+		{	
+			for (int j = 0; j < tokenSet.size(); j++)
+			{
+				if (tokenSet[j].line == i) aotil++;
+			}
+			for (int j = 0; j < aotil; j++)
+			{
+				if (tokenSet[tokenIter + j].token == "=")
+				{
+					if (tokenSet[tokenIter + j - 1].tk_type == 1)
+					{
+						currToken = tokenIter + j + 1;
+						vars.insert({ tokenSet[tokenIter + j - 1].token, isExprss() });
+					}
+				}
+			}
+			tokenIter += aotil;
+			aotil = 0;
+		}
+	}
+
+	double isExprss()
+	{
+		double pom = 0;
+		if (tokenSet[currToken].token == "+")
+		{
+			currToken++;
+			pom = isElem();
+
+		}
+
+		else if (tokenSet[currToken].token == "-")
+		{
+			currToken++;
+			pom = -1 * isElem();
+		}
+		else 
+		{ 
+			//currToken++;
+			pom = isElem(); 
+			//currToken++;
+		}
+		while (currToken < tokenIter + aotil) 
+		{
+			if (tokenSet[currToken].token == "+")
+			{
+				currToken++;
+				pom += isElem();
+			}
+			else if (tokenSet[currToken].token == "-")
+			{
+				currToken++;
+				pom -= isElem();
+			}
+			else if (tokenSet[currToken].token == ")")
+			{
+				//currToken++;
+				return pom;
+			}
+			else currToken++;
+		}
+		return pom;
+	}
+
+	double isElem()
+	{
+		double pom = isFact();
+		currToken++;
+		if (tokenSet[currToken].token == "*")
+		{
+			currToken++;
+			pom = pom * isFact();
+		}
 
 
+		else if (tokenSet[currToken].token == "/")
+		{
+			currToken++;
+			pom = pom / isFact();
+		}
+		return pom;
+	}
 
+	double isFact()
+	{
+		double pom;
+		if (tokenSet[currToken].tk_type == 3)//|| tokenSet[currToken].tk_type == 4|| tokenSet[currToken].tk_type == 5)//num
+		{
+			pom = std::stod(tokenSet[currToken].token.c_str());
+
+		}
+		else if (tokenSet[currToken].tk_type == 1)
+		{ 
+			pom = vars[tokenSet[currToken].token];
+			//currToken++;
+		}
+		else if (tokenSet[currToken].token == "(")
+		{
+			currToken++;
+			pom = isExprss();
+			//if (tokenSet[currToken].token == ")") currToken++;
+			
+
+		}
+		return pom;
+	}
 
 };
--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 
 #include "tokenizer.h"
 #include "compiler.h"
-#include <unordered_map>
+#include <utility>
 
 int main() {
 
@@ -18,15 +18,15 @@ int main() {
 		return -1;
 	}
 	std::cout << "File size: " << tokenizer.buf2Length << std::endl;
-	std::vector<TtokenSet> tokenSet;
-	std::cout << "Class		Line	Pos.	Content" << std::endl;
+	CCompiler comp;
+	//std::cout << "Class		Line	Pos.	Content" << std::endl;
 	while (tokenizer.getToken() == 0 && tokenizer.tk_type != TK_EMPTY)
 	{
 		//if (tokenizer.tk_type != 7) std::cout << TKset[tokenizer.tk_type].ident << "        " << tokenizer.line << "        " << tokenizer.token_start_pos << "        " << tokenizer.token << std::endl;
 		if (tokenizer.tk_type != 7)
 		{
-			printf("%s		%d	%d	%s \n", TKset[tokenizer.tk_type].ident, tokenizer.line, tokenizer.token_start_pos, tokenizer.token);
-			tokenSet.push_back({tokenizer.token, tokenizer.tk_type, tokenizer.line, tokenizer.token_start_pos});
+			//printf("%s		%d	%d	%s \n", TKset[tokenizer.tk_type].ident, tokenizer.line, tokenizer.token_start_pos, tokenizer.token);
+			comp.tokenSet.push_back({ tokenizer.token, tokenizer.tk_type, tokenizer.line, tokenizer.token_start_pos });
 		}
 		if (tokenizer.consumeToken() != 0)
 		{
@@ -37,16 +37,23 @@ int main() {
 
 	tokenizer.Done();
 
-	for (auto a : tokenSet) {
+	std::cout << "Class	Content	Line	Pos." << std::endl;
+	for (auto a : comp.tokenSet) {
 		printf("%d	%s	%d	%d \n", a.tk_type, a.token.c_str(), a.line, a.token_start_pos);
 	}
 
-	std::unordered_map<std::string, double> RANKS = {
-		{"x", 1}, {"y", 2.5}
-	};
+	//std::string variable = "x1";
+	//double value = 2.0;
 
-	std::cout << RANKS["y"] << std::endl;
-	std::cout << RANKS["x"] << std::endl;
+	//comp.vars.insert({variable, value});
+	//std::cout << comp.vars[variable] << std::endl;
+
+	comp.isAssign();
+
+	for (const auto& elem : comp.vars)
+	{
+		std::cout << elem.first << " : " << elem.second << "\n";
+	}
 
 	getchar();
 	return 0;
